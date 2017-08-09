7e0cca21a6997e177fa4929fb854a0d62e7e54af
CHI\mike <mikiej_boy@hotmail.com>
2017-08-09 16:11:26 +0100

    Addede JS folder, js script to index.html and an event handler for moving the mouse over  the main div section

---
+++ TheWorld/wwwroot/js/site.js
@@ -0,0 +1,17 @@
+﻿// site.js
+
+//this script is referenced and called with in index.htm
+var ele = document.getElementById("username");
+ele.innerHTML = "Mikie Jones";
+
+
+//changing background colour of the main section of the index div 
+var main = document.getElementById("main");
+//assign annonymous function
+main.onmouseenter = function () {
+    main.style.backgroundColor = "#888";
+};
+
+main.onmouseleave = function () {
+    main.style.backgroundColor = "";
+};
