b222c5f6b7ddbddaaaf92f3116a80039661d30be
CHI\mike <mikiej_boy@hotmail.com>
2017-08-09 16:19:31 +0100

    Now up to Bower video - added gloabl scope js function to stop variables overwriting one another -

--- TheWorld/wwwroot/js/site.js
+++ TheWorld/wwwroot/js/site.js
@@ -1,17 +1,24 @@
 ﻿// site.js
 
-//this script is referenced and called with in index.htm
-var ele = document.getElementById("username");
-ele.innerHTML = "Mikie Jones";
+
+//wrapping in function hides the global naming issues - i.e all js are executed in global scope and therefore variables could override one another
+(function () {
+
+    //this script is referenced and called with in index.htm
+    var ele = document.getElementById("username");
+    ele.innerHTML = "Mikie Jones";
 
 
-//changing background colour of the main section of the index div 
-var main = document.getElementById("main");
-//assign annonymous function
-main.onmouseenter = function () {
-    main.style.backgroundColor = "#888";
-};
+    //changing background colour of the main section of the index div 
+    var main = document.getElementById("main");
+    //assign annonymous function
+    main.onmouseenter = function () {
+        main.style.backgroundColor = "#888";
+    };
 
-main.onmouseleave = function () {
-    main.style.backgroundColor = "";
-};
+    main.onmouseleave = function () {
+        main.style.backgroundColor = "";
+    };
+
+})();
+//last (); is actually calling this function itself you could always have parenthesis ("hello"); if required. 
