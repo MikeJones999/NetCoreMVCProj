84b332395f2c1299609f5cd13512cbd15f5d98ec
CHI\mike <mikiej_boy@hotmail.com>
2017-08-15 16:39:14 +0100

    Added Mvc via nuget manager - Added developer mode via Startup - config services. Created AppController class as default class. Added MVC Layout pay - as default layout created three pages - index, about and contact and created three views inside View folder

--- TheWorld/Startup.cs
+++ TheWorld/Startup.cs
@@ -16,17 +16,22 @@ namespace TheWorld
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            //set up service container MVC - for dependency injection
+            services.AddMvc();
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
-            //loggerFactory.AddConsole();
+                  
+            loggerFactory.AddConsole();
 
-            //if (env.IsDevelopment())
-            //{
-            //    app.UseDeveloperExceptionPage();
-            //}
+            if (env.IsDevelopment())
+            {
+                //allows dev to see development errors - e.g when 500 error is given - provides stack
+                app.UseDeveloperExceptionPage();
+            }
 
             //app.Run(async (context) =>
             //{
@@ -34,13 +39,23 @@ namespace TheWorld
             //    await context.Response.WriteAsync("<html><body><h3>Hello World!</h3></body></html>");
             //});
 
-            //require this to find and specify the default file
-            app.UseDefaultFiles();
+            //require this to find and specify the default file - if this is what we want to use - in this case index.html
+            //app.UseDefaultFiles();
 
             //allow use of static files from witin the root folder
             app.UseStaticFiles();
 
-            
+            //need middleware MVC to look for and assist with controllers
+            //Handle the default mapping route - much like spring - for incomming requests
+
+            app.UseMvc(config =>
+            {
+                config.MapRoute(
+                    name: "Default",
+                    template: "{controller}/{action}/{id?}",
+                    defaults: new { controller = "App", action = "Index" }
+                    );
+            });
 
         }
     }
